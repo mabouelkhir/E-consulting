f3bfe2f144f77c6d907c460bf99bd5b6d6605c0c
Mohamed Abou El Khir <mohamedabouelkhir34@gmail.com>
2023-07-16 16:24:58 +0100

    first commit

---
+++ src/main/java/com/example/backendstage/BackEndStageApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndStageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendstage/BackEndStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndStageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
